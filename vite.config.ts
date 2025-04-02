1a109547829166bce9a2993b8f28e88eff620e59
Boe Reh <justboereh@gmail.com>
2025-04-02 17:52:39 -0500

    added unoocs

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,24 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import unocss from 'unocss/vite';
+import {
+	transformerDirectives,
+	transformerCompileClass,
+	transformerVariantGroup,
+	presetWind4
+} from 'unocss';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		unocss({
+			presets: [
+				presetWind4({
+					reset: true,
+					dark: 'class'
+				})
+			],
+			transformers: [transformerDirectives(), transformerCompileClass(), transformerVariantGroup()]
+		})
+	]
 });
